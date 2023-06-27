0f25231415885134e93a26f561ccd439fd56772d
shruthihsrinath <shruthihsrinath@users.noreply.github.com>
2023-06-26 20:03:56 -0400

    selection sort

---
+++ selection-sort.js
@@ -0,0 +1,18 @@
+function selectionSort() {
+    let toSortArray = [5, 2, 3, 8, 6];
+    for (let i = 0; i < n; i++) {
+        let tempIndex = i;
+        for (let j = i; j < n; j++) {
+            if (toSortArray[j] < toSortArray[tempIndex]) {
+                tempIndex = j;
+            }
+        }
+        swapFirst(tempIndex, toSortArray, i);
+    }
+}
+
+function swapFirst(index, arr, firstIndex) {
+    let tempVal = arr[index];
+    arr[index] = arr[firstIndex];
+    arr[firstIndex] = tempVal;
+}
